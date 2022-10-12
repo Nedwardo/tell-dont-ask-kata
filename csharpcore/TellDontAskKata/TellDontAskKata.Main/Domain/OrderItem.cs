ab80bd6b201323ffa9cabcba6667d059ed5c1111
root <root@LPT-J8NQ5J3.localdomain>
2022-10-12 18:40:09 +0100

    Prefer own getters and setters to C#
    Signed-off-by: root <root@LPT-J8NQ5J3.localdomain>

--- csharpcore/TellDontAskKata/TellDontAskKata.Main/Domain/OrderItem.cs
+++ csharpcore/TellDontAskKata/TellDontAskKata.Main/Domain/OrderItem.cs
@@ -1,7 +1,23 @@
-﻿namespace TellDontAskKata.Main.Domain
+﻿using System;
+
+namespace TellDontAskKata.Main.Domain
 {
     public class OrderItem
     {
+        public string GetProductName()
+        {
+            return Product.GetName();
+        }
+
+        public decimal GetProductPrice()
+        {
+            return Product.GetPrice();
+        }
+
+        public Product GetProduct()
+        {
+            return Product;
+        }
         public Product Product { get; set; }
         public int Quantity { get; set; }
         public decimal TaxedAmount { get; set; }
