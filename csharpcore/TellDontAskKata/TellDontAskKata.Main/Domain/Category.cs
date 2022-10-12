00217021ebf38fbbb5ca860d70323d8e9d64968e
root <root@LPT-J8NQ5J3.localdomain>
2022-10-12 15:46:15 +0100

    Moved Category initalisation to initaliiser
    Signed-off-by: root <root@LPT-J8NQ5J3.localdomain>

--- csharpcore/TellDontAskKata/TellDontAskKata.Main/Domain/Category.cs
+++ csharpcore/TellDontAskKata/TellDontAskKata.Main/Domain/Category.cs
@@ -2,6 +2,11 @@
 {
     public class Category
     {
+        public Category(string name, decimal taxPercentage)
+        {
+            Name = name;
+            TaxPercentage = TaxPercentage;
+        }
         public string Name { get; set; }
         public decimal TaxPercentage { get; set; }
     }
